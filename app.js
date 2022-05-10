c92d0abdb82cc3282a70718fab5380bcd7ff8ca4
sourmix <75124027+sourmix@users.noreply.github.com>
2022-05-10 18:51:46 +0900

    cavas initial

---
+++ app.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById("jsCanvas");
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  console.log(x, y);
+}
+
+function onMouseDown(event) {
+  console.log(event);
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+}
