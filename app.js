58fc0f86067a7a9dfd03d7caea7612d314777804
sourmix <75124027+sourmix@users.noreply.github.com>
2022-05-11 17:56:16 +0900

    second commit

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+canvas.width = 700;
+canvas.height = 700;
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+function startPainting() {
+  painting = true;
+}
 
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  console.log(x, y);
-}
-
-function onMouseDown(event) {
-  console.log(event);
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
 }
